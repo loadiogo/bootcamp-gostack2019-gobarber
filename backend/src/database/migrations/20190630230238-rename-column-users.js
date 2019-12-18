07ab32e0d98bd76c20753fa706dbf091622fe11f
Diogo Henrique Silva <diogo.hglima@gmail.com>
2019-12-18 19:31:17 -0300

    [fix] fixes problems with eslint and add new pages

--- backend/src/database/migrations/20190630230238-rename-column-users.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  up: queryInterface => {
-    return queryInterface.renameColumn('users', 'nome', 'name');
-  },
-
-  down: queryInterface => {
-    return queryInterface.renameColumn('users', 'name', 'nome');
-  },
-};
