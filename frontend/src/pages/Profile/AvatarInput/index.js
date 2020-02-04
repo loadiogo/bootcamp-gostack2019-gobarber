da19355075a703eee2af857513df4522f5a75990
Diogo Henrique Silva <diogo.hglima@gmail.com>
2020-02-03 23:26:33 -0300

    [feature] add profile picture

---
+++ frontend/src/pages/Profile/AvatarInput/index.js
@@ -0,0 +1,32 @@
+import React, { useState, useRef } from 'react';
+import { useField } from '@rocketseat/unform';
+
+import { Container } from './styles';
+
+export default function AvatarInput() {
+  const { defaultValue, registerField } = useField('avatar');
+
+  const [file, setFile] = useState(defaultValue && defaultValue.id);
+  const [preview, setPreview] = useState(defaultValue && defaultValue.url);
+
+  const ref = useRef();
+
+  function handleChange(e) {}
+
+  return (
+    <Container>
+      <label htmlFor="avatar">
+        <img src="" alt="" />
+
+        <input
+          type="file"
+          id="avatar"
+          accept="image/*"
+          data-file={file}
+          onChange={handleChange}
+          ref={ref}
+        />
+      </label>
+    </Container>
+  );
+}
